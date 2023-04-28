77b940e26dc2b725f365468dca7a207ba411fe19
Sarvex <sarvex@jatasra.com>
2023-04-28 11:38:38 +0530

    Added Jost font

--- vite.config.ts
+++ vite.config.ts
@@ -4,19 +4,25 @@ import { qwikCity } from '@builder.io/qwik-city/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { partytownVite } from '@builder.io/partytown/utils';
 import { join } from 'path';
+import Unfonts from 'unplugin-fonts/vite'
 
 export default defineConfig(() => {
   return {
-    plugins: [
-      qwikCity(),
-      qwikVite(),
-      tsconfigPaths(),
-      partytownVite({ dest: join(__dirname, 'dist', '~partytown') }),
-    ],
-    preview: {
-      headers: {
-        'Cache-Control': 'public, max-age=600',
-      },
-    },
-  };
+			plugins: [
+				qwikCity(),
+				qwikVite(),
+				tsconfigPaths(),
+				partytownVite({ dest: join(__dirname, "dist", "~partytown") }),
+				Unfonts({
+					google: {
+						families: [{name: 'Jost', styles: 'ital,wght@0,400;1,200',}],
+					},
+				}),
+			],
+			preview: {
+				headers: {
+					"Cache-Control": "public, max-age=600",
+				},
+			},
+		};
 });
